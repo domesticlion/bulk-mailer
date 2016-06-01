65428b9338fc9a9475ba6c5ad93d0825db252106
domesticlion <domesticlion@gmail.com>
2016-06-02 07:56:24 +0900

    init

---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'mandrill' => [
+        'secret' => env('MANDRILL_SECRET'),
+    ],
+
+    'ses' => [
+        'key'    => env('AWS_SES_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SES_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_SES_REGION'),
+    ],
+
+    'stripe' => [
+        'model'  => App\User::class,
+        'key'    => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+];
